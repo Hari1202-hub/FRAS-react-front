eb8c710dc3d56d16627a028acfe4f103f5bf8f6d
MimicAk <muthurose2204@gmail.com>
2026-06-06 13:52:19 +0530

    Point API base URL to 3.88.178.47

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ import "./bootstrap";
 // export const BASEURL = 'http://127.0.0.1:8000/api/';
 
 // staging
-export const BASEURL = "http://44.204.69.221/api/";
-export const BASEPATH = "http://44.204.69.221/";
+// export const BASEURL = "http://44.204.69.221/api/";
+// export const BASEPATH = "http://44.204.69.221/";
 
 // production
-// export const BASEURL = 'http://3.88.178.47/api/';
-// export const BASEPATH = 'http://3.88.178.47/';
+export const BASEURL = 'http://3.88.178.47/api/';
+export const BASEPATH = 'http://3.88.178.47/';
 export const BASENAME = "/";
 export const TOKEN = () => localStorage.getItem("access_token") || "";
